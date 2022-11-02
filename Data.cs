cdc880753cda3afe37761e7876f8c60bf148a4d3
Bureš Tomáš <bures.tomas@live.com>
2022-11-02 22:53:32 +0100

    spaghetti magic

---
+++ Data.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WebCrawlerWatchDog
+{
+	public class CrawlerConfig
+	{
+		public CrawlPage[] UrlArray { get; set; }
+		public StatsData[] StoreDefinitions { get; set; }
+		public string[] UnavailableKeywords { get; set; }
+		public MailSettings MailSettings { get; set; }
+	}
+
+	public class StatsData
+	{
+		public string Name { get; set; }
+		public string Sequence { get; set; }
+		[JsonIgnore]
+		public string Result { get; set; }
+	}
+
+	public class CrawlPage
+	{
+		public string Url { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class MailSettings
+	{
+		public string SmtpServer { get; set; }
+		public int Port { get; set; }
+		public string SmtpUsername { get; set; }
+		public string SmtpPassword { get; set; }
+		public string Sender { get; set; }
+		public string Recipient { get; set; }
+		public string Subject { get; set; }
+		public string FoundSubjectPrefix { get; set; }
+		public bool EnableSsl { get; set; }
+	}
+}
--- WebCrawler.cs
+++ WebCrawler.cs
@@ -1,89 +1,157 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Web;
 
 namespace WebCrawlerWatchDog
 {
 	internal class WebCrawler
 	{
-		public class StatsData
-		{
-			public string Name { get; set; }
-			public string Sequence { get; set; }
-			public string Result { get; set; }
-		}
-
 		public static void Run()
 		{
-			StatsData[] stores = new StatsData[]
-			{
-				new StatsData
-				{
-					Name = "Na Příkopě 820/24, Praha 1",
-					Sequence = "820/24, Praha 1"
-				},
-				new StatsData
-				{
-					Name = "Havelská 503/19, Praha 1",
-					Sequence = "503/19, Praha 1"
-				},
-				new StatsData
-				{
-					Name = "Roosveltova 419/20, Brno - střed",
-					Sequence = "419/20, Brno - střed"
-				},
-				new StatsData
-				{
-					Name = "Mírové náměstí 15, Jablonec nad Nisou",
-					Sequence = "15, Jablonec nad Nisou"
-				},
-				new StatsData
-				{
-					Name = "Suché Mýto 1, Bratislava",
-					Sequence = "1, Bratislava"
-				}
-			};
-
-			Console.WriteLine("ceskamincovna.cz watchdog crawler v1.0");
+			StringBuilder sb = new StringBuilder();
+			StringBuilder log = new StringBuilder();
+			Console.WriteLine("ceskamincovna.cz watchdog crawler v1.1");
+			sb.AppendLine("ceskamincovna.cz watchdog crawler v1.1");
 			Console.WriteLine("--------------------------------------");
-			string url = "https://ceskamincovna.cz/stribrna-mince-psi-plemena---border-kolie-proof-389-14127-d";
-			string response = getResponse(url);
-			response = WebUtility.HtmlDecode(response);
-			//Console.WriteLine(response);
-			int dostupna = 0;
-			int nedostupna = 0;
-			foreach (StatsData s in stores)
+			sb.AppendLine("--------------------------------------");
+			sb.AppendLine();
+
+			bool isAnyCoinAvailable = false;
+
+
+			#region inicializace a kontroly configu
+			if (!File.Exists("config.json"))
 			{
-				string status = findAwail(response, s.Sequence);
-				Console.WriteLine(s.Name + " - " + status);
-				if (status == "nedostupný")
-					nedostupna++;
-				else
-					dostupna++;
+				Console.WriteLine("CONF_ERROR - Nenalezen config file");
+				return;
 			}
-			Console.WriteLine("--------------------------------------");
-			Console.WriteLine($"Dostupna: {dostupna} Nedostupna: {nedostupna}");
-			Console.ReadKey();
-		}
 
-		static string findBetween(string s1, string s2, string data)
-		{
-			int num = data.IndexOf(s1) + s1.Length;
-			int num2 = data.IndexOf(s2, num);
-			return data.Substring(num, num2 - num);
+			var configText = File.ReadAllText("config.json");
+			CrawlerConfig crawlerConfig = null;
+			if (!string.IsNullOrEmpty(configText))
+			{
+				crawlerConfig = JsonConvert.DeserializeObject<CrawlerConfig>(configText);
+			}
+
+			if (crawlerConfig == null)
+			{
+				Console.WriteLine("CONF_ERROR - Nezdarila se deserializace configu");
+				return;
+			}
+
+			if (crawlerConfig.UrlArray == null || crawlerConfig.UrlArray.Length <= 0)
+			{
+				Console.WriteLine("CONF_ERROR - Nezadany zadne url ke kontrole");
+				return;
+			}
+
+			if (crawlerConfig.StoreDefinitions == null || crawlerConfig.StoreDefinitions.Length <= 0)
+			{
+				Console.WriteLine("CONF_ERROR - Nezadany prodejny ke kontrole");
+				return;
+			}
+
+			#endregion
+
+			foreach (var page in crawlerConfig.UrlArray)
+			{
+				Console.WriteLine($"{page.Name ?? "UNK page"}");
+				log.AppendLine($"{page.Name ?? "UNK page"}");
+				try
+				{
+					string response = getResponse(page.Url);
+					if (string.IsNullOrEmpty(response))
+						throw new ApplicationException("	ERROR - invalid web response");
+
+					response = WebUtility.HtmlDecode(response);
+
+					foreach (StatsData s in crawlerConfig.StoreDefinitions)
+					{
+						string status = findAwail(response, s.Sequence);
+						if (string.IsNullOrEmpty(status))
+						{
+							Console.WriteLine($"	WARNING - Pro obchod [{s.Name ?? "undefined"}] - nenalezen status!");
+							log.AppendLine($"	WARNING - Pro obchod [{s.Name ?? "undefined"}] - nenalezen status!");
+						}
+						Console.WriteLine($"	{s.Name ?? "undefined"} - {status}");
+						log.AppendLine($"	{s.Name ?? "undefined"} - {status}");
+
+						bool isAvail = true;
+						foreach (var uKey in crawlerConfig.UnavailableKeywords)
+						{
+							if (status.ToLower().Contains(uKey.ToLower()))
+							{
+								isAvail = false; 
+								break;
+							}
+						}
+
+						if (isAvail)
+						{
+							isAnyCoinAvailable = true;
+							sb.Append($" SKLADEM! Mince: {page.Name ?? "unk_page_name"} Obchod: {s.Name ?? "unk"} Stav: {status}");
+							sb.AppendLine();
+						}
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine(ex.Message);
+					log.AppendLine(ex.Message);
+				}
+			}
+
+			sb.AppendLine();
+			sb.AppendLine();
+			sb.AppendLine();
+			sb.AppendLine("--- LOG ---");
+			sb.Append(log);
+
+			if (!string.IsNullOrEmpty(crawlerConfig.MailSettings?.SmtpServer))
+			{
+				Console.WriteLine("Sending mail");
+				try
+				{
+					var smtpClient = new SmtpClient(crawlerConfig.MailSettings?.SmtpServer, crawlerConfig.MailSettings?.Port > 0 ? crawlerConfig.MailSettings.Port : 587)
+					{
+						Credentials = new NetworkCredential(crawlerConfig.MailSettings?.SmtpUsername ?? "unknown", crawlerConfig.MailSettings?.SmtpPassword ?? "unknown"),
+						EnableSsl = crawlerConfig.MailSettings?.EnableSsl ?? false //pokud nebude v JSONU je default false!
+					};
+
+					string subject = crawlerConfig.MailSettings?.Subject ?? String.Empty;
+					if (isAnyCoinAvailable)
+						subject = (crawlerConfig.MailSettings?.FoundSubjectPrefix ?? String.Empty) + " " + subject;
+
+					smtpClient.Send(crawlerConfig.MailSettings?.Sender ?? "unknow", crawlerConfig.MailSettings?.Recipient ?? "unknown", subject, sb.ToString());
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine("ERROR sending email: " + ex.Message ?? String.Empty);
+				}
+				Console.WriteLine("Mail sent!");
+			}
 		}
 
 		static string findAwail(string response, string store_name)
 		{
 			int store_index = response.IndexOf(store_name);
+			if (store_index < 0)
+				return null;
 			int i1 = response.IndexOf("</div>", store_index);
+			if (i1 < 0)
+				return null;
 			int i2 = response.IndexOf("</div>", i1 + 6);
+			if (i2 < 0)
+				return null;
 			int i3 = response.Substring(0, i2 - 6).LastIndexOf('>');
+			if (i3 < 0)
+				return null;
 			return response.Substring(i3+1, i2 - i3 - 1).Replace("\r", String.Empty).Replace("\n", String.Empty).Trim();
 		}
 
@@ -93,6 +161,8 @@ namespace WebCrawlerWatchDog
 			httpWebRequest.Method = "GET";
 			httpWebRequest.Headers.Add(HttpRequestHeader.AcceptCharset, "utf-8");
 			HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+			if ((int)httpWebResponse.StatusCode < 200 || (int)httpWebResponse.StatusCode >= 300)
+				throw new ApplicationException($"	ERROR loading page: {(int)httpWebResponse.StatusCode} response code ");
 			return new StreamReader(httpWebResponse.GetResponseStream(), Encoding.UTF8).ReadToEnd();
 		}
 	}
